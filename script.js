86e6b4879890e2d92f2afff65ee0670bede15378
Cristiano Rasweiler Neto <cristianorneto@gmail.com>
2021-06-05 15:51:22 -0300

    creating object

---
+++ script.js
@@ -0,0 +1,40 @@
+<script>
+
+const post = [
+{
+    nome: "gi.almerindo",
+    foto: "imagem",
+    curtido: "augustawillrich",
+    texto: "#tbt 👑👑Eu sempre achei elas parecidas mas uma é rainha de lá e outra aqui do lado de dentro 💛! Quanta saudade, vó amada!",
+    comentarista: "heloisyin93",
+    quantotempopostou: 19
+},
+{
+    nome: "ruiter_trader",
+    foto: "imagem",
+    curtido: "investidor",
+    texto: "JA REFLETIU SOBRE ISSO?! #analisetecnica #swingtrading #swingtrader #daytrade #daytraderbrasil #rendavariavel...,
+    comentarista: "fabioholder",
+    quantotempopostou: 23
+
+},
+{
+    nome: "sejaumapessoamelhor",
+    foto: "imagem",
+    curtido: "augustawillrich",
+    texto: "Quantas vezes você já machucou alguém — ou alguém te machucou — por palavras ditas nos piores momentos?...",
+    comentarista: "heloisyin93",
+    quantotempopostou: 
+},
+]
+
+const 
+<ul>
+<li>
+  <img src="https://avatars.githubusercontent.com/u/79769515?v=4"></img>
+
+  <a class="gizelle" href="/gi.almerindo/" tabindex="0"><strong>gi.almerindo</strong></a>
+</li>
+</ul>
+
+</script>
